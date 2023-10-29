7bebe68d5fb4f803f2885ec9c431cfd168511fcb
Charlilyyyy <mohamadtaufik099@gmail.com>
2023-10-30 00:00:34 +0800

    verify otp

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -27,4 +27,29 @@ class LoginController extends Controller
         //return back a response
         return response()->json(['message' => 'Text message notificaation sent. ']);
     }
+
+    public function verify(Request $request){
+        //validate incoming request
+        $request->validate([
+            'phone' => 'required|numeric|min:10',
+            'login_code' => 'required|numeric|between:111111,999999'
+        ]);
+
+        //find user
+        $user = User::where('phone', $request->phone)
+                    ->where('login_code', $request->login_code)
+                    ->first();
+
+        //is code provided match with the one saved?
+        //if yes, return auth token
+        if($user){
+            $user->update([
+                'login_code' => null
+            ]);
+            return $user->createToken($request->login_code)->plainTextToken;
+        }
+
+        //if no , return login with a message
+        return response()->json(['message' => 'Invalid verification code.'], 401);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\loginController;
 
 Route::post('/login',[loginController::class, 'submit']);
+Route::post('/login/verify',[loginController::class, 'verify']);
